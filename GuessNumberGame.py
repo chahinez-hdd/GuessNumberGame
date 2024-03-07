6ed50a148a7dd29a81433cb89e5e2bbc166c952a
chahinez-hdd <chahinez.haddad2005@gmail.com>
2024-03-07 21:11:37 +0100

    version1

---
+++ GuessNumberGame.py
@@ -0,0 +1,20 @@
+import random
+i=random.randint(0,100)
+
+for j in range(0,10):
+    x=input()
+    if int(x)==int(i) :
+        j+=1
+        break
+    
+    if int(x)<int(i) :
+        print("le nombre est plus petit! ")
+    else  :
+        print("le nombre est plus grand! ")
+
+
+if int(x)==int(i) :
+    print("Nombre trouvé!!")
+
+if j==9 :
+    print("\nNombres de tentatives terminé :(")
